4342dab9bb7ee187ff69cb9549df270144a5659b
Piyush Garg <gargpiyush195@gmail.com>
2020-05-31 17:17:35 +0530

    Cors Added

--- index.ts
+++ index.ts
@@ -1,10 +1,10 @@
 import { Application } from "https://deno.land/x/abc@v1.0.0-rc8/mod.ts";
 import { getAllBlogs, getBlogById, addNewBlog } from './controllers/index.ts';
-import { oakCors } from "https://deno.land/x/cors/mod.ts";
+import { abcCors } from "https://deno.land/x/cors/mod.ts";
 
 const app = new Application();
 
-app.use(oakCors());
+app.use(abcCors());
 
 app
 .get('/blogs', getAllBlogs)
